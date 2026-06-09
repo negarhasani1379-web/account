cf6bd272e7bafb92c89349cfc437b578e9044f47
Negar <negarhasani1379@gmail.com>
2026-06-09 16:55:55 +0330

    write views of accounts and clean gitignore

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from django.contrib.auth.views import (LoginView,LogoutView)
+from django.views.generic import CreateView
+from .forms import SignupForm
+class SignupView(CreateView):
 
-# Create your views here.
+    model = User
+    form_class = SignupForm
+    template_name = 'accounts/signup.html'
+    success_url = reverse_lazy('login')
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
+class CustomLogoutView(LogoutView):
+    next_page = 'login'
+

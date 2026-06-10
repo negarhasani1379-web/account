a2eed8f25f30335469d989d00669015f02da1f1d
Negar <negarhasani1379@gmail.com>
2026-06-10 14:58:48 +0330

    clean

--- people/models.py
+++ people/models.py
@@ -8,19 +8,13 @@ class Person(models.Model):
     national_code = models.CharField(
         max_length=10,
         unique=True,
-        db_index=True
-    )
+        db_index=True)
 
     phone_number = models.CharField(max_length=13,unique=True,db_index=True)
-
     father_name = models.CharField(max_length=100)
-
     birth_date = models.DateField()
-
     address = models.TextField()
-
     created_at = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
